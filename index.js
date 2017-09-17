35b3e3db0accd5d876ed634e05c5c9cd02feba57
Sherry <sherryptk@gmail.com>
2017-09-17 15:50:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,4 +19,16 @@ function destructivelyRemoveFirstKitten(){
   kittens.shift()
   return kittens
 }
+
+function appendKitten(name){
+  var moreKittens[name, ...kittens]
+  return moreKittens
+}
+
+// var cities = ["New York", "San Francisco"]
+//  
+// ["Philadelphia", ...cities] // ["Philadelphia", "New York", "San Francisco"]
+//  
+// cities // ["New York", "San Francisco"]
+
 //pop() - removes from end,  shift() - remove from start, and unshift() - add to start, push() - add to end,
