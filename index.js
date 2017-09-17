b7ef810d1908513e21e735079fc867653103f927
Sherry <sherryptk@gmail.com>
2017-09-17 15:13:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function destructivelyRemoveFirstKitten(array){
   array.shift()
   return array
 }
+function destructivelyAppendKitten(name){
+  array.push(name)
+  return array
+}
+
+
+
+//pop() - removes from end, push() - add to end, shift() - remove from start, and unshift() - add to start
